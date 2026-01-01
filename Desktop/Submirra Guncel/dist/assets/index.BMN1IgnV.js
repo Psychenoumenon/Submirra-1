e71f327b184707ca6646fa54b75ee3957c490db4
Your Name <your.email@example.com>
2026-01-01 13:13:02 +0300

    dist klasörü güncellendi

--- Desktop/Submirra Guncel/dist/assets/index.BMN1IgnV.js
+++
@@ -1,1 +0,0 @@
-import{r as i}from"./index.v56SApCw.js";const t=i("PushNotifications",{});export{t as PushNotifications};
---
+++ Desktop/Submirra Guncel/dist/assets/index.CwhMnUbW.js
@@ -0,0 +1,1 @@
+import{r as i}from"./index.6sp-nx5z.js";const t=i("PushNotifications",{});export{t as PushNotifications};
